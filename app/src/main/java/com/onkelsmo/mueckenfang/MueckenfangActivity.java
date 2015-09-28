d7204058c0f7607315795edbb52baf61de01115a
Jan <onkelsmo@gmail.com>
2015-09-28 16:47:25 +0200

    ADD: game activity and layout for main activity

--- app/src/main/java/com/onkelsmo/mueckenfang/MueckenfangActivity.java
+++ app/src/main/java/com/onkelsmo/mueckenfang/MueckenfangActivity.java
@@ -1,13 +1,22 @@
 package com.onkelsmo.mueckenfang;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MueckenfangActivity extends Activity {
-
+public class MueckenfangActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, GameActivity.class));
     }
 }
