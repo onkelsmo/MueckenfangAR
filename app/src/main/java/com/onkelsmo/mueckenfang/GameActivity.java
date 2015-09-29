48ea29c4930de43d1a6269e3972b17c7f3f22cfb
Jan <onkelsmo@gmail.com>
2015-09-29 16:39:18 +0200

    ADD: updateDisplay Method

--- app/src/main/java/com/onkelsmo/mueckenfang/GameActivity.java
+++ app/src/main/java/com/onkelsmo/mueckenfang/GameActivity.java
@@ -2,6 +2,9 @@ package com.onkelsmo.mueckenfang;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class GameActivity extends Activity {
     private boolean isRuning = false;
@@ -10,11 +13,13 @@ public class GameActivity extends Activity {
     private int midges;
     private int midgesCatched;
     private int time;
+    private float scale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+        scale = getResources().getDisplayMetrics().density;
         startGame();
     }
 
@@ -32,7 +37,27 @@ public class GameActivity extends Activity {
         time = 60;
         updateDisplay();
     }
-    private void updateDisplay() {
 
+    private void updateDisplay() {
+        TextView tvPoints = (TextView)findViewById(R.id.points);
+        tvPoints.setText(Integer.toString(score));
+
+        TextView tvRound = (TextView)findViewById(R.id.round);
+        tvRound.setText(Integer.toString(round));
+
+        TextView tvHits = (TextView)findViewById(R.id.hits);
+        tvHits.setText(Integer.toString(midgesCatched));
+
+        TextView tvTime = (TextView)findViewById(R.id.time);
+        tvTime.setText(Integer.toString(time));
+
+        FrameLayout flHits = (FrameLayout)findViewById(R.id.bar_hits);
+        FrameLayout flTime = (FrameLayout)findViewById(R.id.bar_time);
+
+        LayoutParams lpHits = flHits.getLayoutParams();
+        lpHits.width = Math.round(scale * 300 * Math.min(midgesCatched,midges) / midges);
+
+        LayoutParams lpTime = flTime.getLayoutParams();
+        lpTime.width = Math.round(scale * time * 300 / 60);
     }
 }
