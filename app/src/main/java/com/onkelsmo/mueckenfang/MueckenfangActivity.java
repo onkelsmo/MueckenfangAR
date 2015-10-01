9e90dfcf8eb80f97e799969d14e69db03b5a5f49
Jan <onkelsmo@gmail.com>
2015-10-01 20:10:51 +0200

    ADD: fade in animation

--- app/src/main/java/com/onkelsmo/mueckenfang/MueckenfangActivity.java
+++ app/src/main/java/com/onkelsmo/mueckenfang/MueckenfangActivity.java
@@ -4,15 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MueckenfangActivity extends Activity implements View.OnClickListener {
+    private Animation fadeInAnimation;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View view = findViewById(R.id.root);
+        view.startAnimation(fadeInAnimation);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+        fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
     }
 
     @Override
